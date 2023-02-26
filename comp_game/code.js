72e945479e7be288d37939ea55f2d826d5c516f2
rarechro <paytonjanko@gmail.com>
2023-02-25 19:21:09 -0700

    display users data from a form

---
+++ comp_game/code.js
@@ -0,0 +1,51 @@
+
+/*
+
+var form = document.getElementById("myForm");
+
+
+
+
+
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  form.addEventListener("submit", function(event) {
+    // prevent the default form submission behavior
+    event.preventDefault();
+  
+    userN = form.username.value;
+    userWep = form.Weapons.value;
+    userH_D = form.Health/Damage.value;
+    userP = form.TotalPoints.value;
+  
+    console.log(userN, userWep, userH_D, userP);
+  
+  
+  });
+});
+
+
+function statBuilder(charBuilder){
+  var userN = document.getElementById("username");
+  var userWep = document.getElementById("Weapons");
+  var userH_D = document.getElementById("Health/Damage");
+  var userP = document.getElementById("TotalPoints");
+  
+  
+  
+  
+  
+  userN = charBuilder.username.value;
+  userWep = charBuilder.Weapons.value;
+  userH_D = charBuilder.Health/Damage.value;
+  userP = charBuilder.TotalPoints.value;
+ 
+  console.log(userN, userWep, userH_D, userP);
+
+
+}
+ */
---
+++ comp_game/readme.txt
@@ -0,0 +1,7 @@
+*I decided to go with inline js and css because external js was not working properly*
+
+index.html makes up the entirety of this project. It builds a table based on user inputs and displays them.
+The JS in index.html ensures the data is loaded then stops the website from refreshing which is a defailt attribute of the submit button on forms.
+this allows me to extract the user input from the form and build a table around the information.
+I then append the table to the body of the html page with the users data.
+styles.css was not used in this project.*I used inline CSS*
